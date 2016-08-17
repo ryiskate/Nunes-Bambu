18a76153a5865a87846788210504319ecd6db1f7
Ricardo Ide <ryiskate@yahoo.com.br>
2016-08-17 02:09:12 +0000

    activeadmin resources

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -7,10 +7,23 @@ ActiveAdmin.register Product do
 #
 # or
 #
-  permit_params do
-    permitted = [:category_id, :name, :price, :description]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
+
+permit_params :category_id, :name, :price, :description, images_attributes: [:id, :img]
+
+form do |f|
+    f.inputs do
+      input :category
+      input :name
+      input :description
+      input :price
+    end
+    
+    f.has_many :images do |i|
+      i.input :img, as: :file
+    end
+      
+    f. actions
+    
   end
 
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :images
+  
+  accepts_nested_attributes_for :images, :allow_destroy => true
+  
 end
